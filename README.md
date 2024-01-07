43f8345cc02736e1911d7b49cd13b4625224b616
matinkzm <kazemi99.itsu@gmail.com>
2024-01-07 10:18:38 +0330

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 2-movie-data
+
+This is second project from this link : https://medium.com/@luisprooc/30-data-engineering-project-ideas-9ecf0a70cbea
+
+In this project I created a small ETL data pipeline for movie data.
+
+First I read the csv file , select the data i wanted and move it to dataframe and then into a csv file.
+
+Extract = read the csv file
+Transform = select movie name , year and its rating
+load = movie it to the dataframe and then csv file
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+# second project from this link : https://medium.com/@luisprooc/30-data-engineering-project-ideas-9ecf0a70cbea
 import csv
 import pandas as pd
 
+# create an empty data frame
 movie_dataframe = pd.DataFrame(columns=['movie name', 'year', 'rating'])
-
+# open and read the csv file
 with open('imdb_top_250.csv') as file:
+    # skip the first line because it is column names
     next(file)
     reader = csv.reader(file)
     index = 0
     for line in reader:
         str_line = str(line)
+        # separate the data with comma
         temp_str = [s.strip() for s in str_line.split(',')]
+        # add name , year , movie rating into the dataframe
         movie_dataframe.loc[index] = [temp_str[1]] + [temp_str[2]] + [temp_str[7]]
         index += 1
 
-
+# movie the dataframe into a csv file
 movie_dataframe.to_csv("final.csv")
